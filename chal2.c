54d47e63d3dda03ded24add453f63e0cd5292b4a
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2021-11-22 10:24:19 +0100

    ajout

--- chal2.c
+++ chal2.c
@@ -3,17 +3,18 @@
   
 void Echange (float NUM1, float NUM2){
   float n;
- 
+ printf("Entrez une valeur pour NUM1 et NUM2 : ");
+   scanf("%f %f", &NUM1 , &NUM2);
     n=NUM1;
     NUM1=NUM2;
     NUM2=n;
+
+printf("Les valeurs apres echange sont : NUM1=%f et NUM2=%f", NUM1, NUM2);
 }
+
 int main()
    {
-       int NUM1, NUM2;
-      printf("Entrez une valeur pour NUM1 et NUM2 : ");
-      scanf("%f %f", &NUM1 , &NUM2);
-      Echange(NUM1, NUM2);
-      printf("Les valeurs apres echange sont : NUM1=%f et NUM2=%f", NUM1, NUM2);
-      return 0;
+      
+      Echange();
+     
    }
